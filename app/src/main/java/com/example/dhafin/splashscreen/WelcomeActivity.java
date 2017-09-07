301e8ad3210d204265056c2eb1987539674453e2
Dhafin Rizqullah <dhafinriz050500@gmail.com>
2017-09-07 19:20:28 +0700

    First Project

---
+++ app/src/main/java/com/example/dhafin/splashscreen/WelcomeActivity.java
@@ -0,0 +1,138 @@
+package com.example.dhafin.splashscreen;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class WelcomeActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private ViewPager mPager;
+    private int[] layout={R.layout.first_slide,R.layout.second_slide,R.layout.third_slide,
+    R.layout.fourth_slide};
+    private MpagerAdapter mpagerAdapter;
+
+    private LinearLayout Dots_Layout;
+    private ImageView[] dots;
+
+    private Button BnNext,BnSkip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(new PreferenceManager(this).checkPreference())
+        {
+            loadHome();
+        }
+        if(Build.VERSION.SDK_INT>=15)
+        {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        else
+        {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        setContentView(R.layout.activity_welcome);
+
+        mPager=(ViewPager)findViewById(R.id.viewPager);
+        mpagerAdapter=new MpagerAdapter(layout,this);
+        mPager.setAdapter(mpagerAdapter);
+        Dots_Layout=(LinearLayout)findViewById(R.id.dotsLayout);
+        BnNext=(Button)findViewById(R.id.bnNext);
+        BnSkip=(Button)findViewById(R.id.bnSkip);
+        BnNext.setOnClickListener(this);
+        BnSkip.setOnClickListener(this);
+        createDots(0);
+
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                createDots(position);
+                if(position==layout.length-1)
+                {
+                    BnNext.setText("Start");
+                    BnSkip.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    BnNext.setText("Next");
+                    BnSkip.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+    private void createDots(int current_position)
+    {
+        if (Dots_Layout!=null)
+            Dots_Layout.removeAllViews();
+
+        dots=new ImageView[layout.length];
+        for(int i=0;i<layout.length;i++)
+        {
+            dots[i]=new ImageView(this);
+            if (i==current_position)
+            {
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this,R.drawable.active_dots));
+            }
+            else
+            {
+                dots[i].setImageDrawable(ContextCompat.getDrawable(this,R.drawable.default_dots));
+            }
+            LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(4,0,4,0);
+
+            Dots_Layout.addView(dots[i],params);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.bnNext:
+                loadNextSlide();
+                break;
+            case R.id.bnSkip:
+                loadHome();
+                new PreferenceManager(this).writePreference();
+                break;
+        }
+    }
+    private void loadHome(){
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+    private void loadNextSlide()
+    {
+        int next_slide=mPager.getCurrentItem()+1;
+        if(next_slide<layout.length)
+        {
+            mPager.setCurrentItem(next_slide);
+        }
+        else
+        {
+            loadHome();
+            new PreferenceManager(this).writePreference();
+        }
+    }
+}
